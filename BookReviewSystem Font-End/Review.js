722007b9b9d0b683cc43e31710f725475e08752b
AmaraNK9 <naymyo1310mdy@gmail.com>
2023-06-23 16:09:56 +0630

    original

--- BookReviewSystem Font-End/Review.js
+++ BookReviewSystem Font-End/Review.js
@@ -45,3 +45,51 @@
     }
   }
   
+  $(document).ready(function(){
+     
+    var offset = 4; 
+    var limit = 4;
+
+    $('#loadMoreBtn').on('click',function() {
+        $.ajax({
+            type: "POST",
+            url: "load_more_reviews.php", 
+            data: {
+                offset: offset,
+                limit: limit,
+            },
+            success: function(response) {
+                console.log(response)
+                var reviews = $.parseJSON(response);
+                var authorContainer = $('main');
+
+                if (reviews.length > 0) {
+                    $.each(reviews, function(index, review) {
+                        var authorCard = '<div class="col-md-3 sm-4 mb-3 originalauthors">' +
+                            '<div class="card-parent">' +
+                            '<div class="author-card">' +
+                            '<img class="author-image card-img-top" src="../image/' + review.image + '" alt="Author Image">' +
+                            '<h2 class="author-name">' + review.name + '</h2>' +
+                            '<p class="author-bio">Author Bio</p>' +
+                            '<a class="author-website"  href="AuthorDetail.php?id=' + review.id + '" target="_blank">Author\'s Books</a>' +
+                            '</div>' +
+                            '</div>' +
+                            '</div>';
+
+                        authorContainer.append(authorCard);
+                    });
+
+                    offset += limit; 
+                    
+                }
+                
+                if(reviews.length<3)               
+                {
+                    console.log(authors.length)
+                    $('#loadMoreBtn').hide(); 
+                }
+            }
+        });
+        
+    });
+})
