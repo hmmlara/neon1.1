d893aeebaa34ca00f896a4424aadba0bf491a409
saihtoolwin <saihtoolwin66@gmail.com>
2023-06-29 14:13:52 +0630

    change booksselect

--- BookReviewSystem Font-End/getcategory.php
+++ BookReviewSystem Font-End/getcategory.php
@@ -6,6 +6,22 @@ $review_model = new Reviews();
 $getSelectCategory=new CategoryController();
 //$value=$_POST['value'];
 $getCategory=$getSelectCategory->getUserSelectCategory(1);
+
+$result = [];
+foreach($getCategory as $book){
+    // var_dump($book);
+    $auther_name = $review_model->get_author_by_id($book['auther_id']);
+    if($auther_name){
+        // var_dump($auther_name['name']);
+        $auther_name_arr = array("auther_name"=>$auther_name['name']);
+        $book+= $auther_name_arr;
+    }else{
+        $auther_name_arr = array("auther_name"=>"Anonymous");
+        $book+= $auther_name_arr;
+    }
+ 
+    $result[] = $book;
+}
 var_dump($getCategory);
 //echo $getCategory;
 
