75bdd97e347e9ead3eee9557701ce2c1976c3a47
AmaraNK9 <naymyo1310mdy@gmail.com>
2023-06-30 10:50:35 +0630

    change nav-bar style

--- BookReviewSystem Font-End/nav.php
+++ BookReviewSystem Font-End/nav.php
@@ -1,14 +1,14 @@
 <!-- Navigation bar -->
 <nav style="width:100%;top :0px;z-index:99;box-shadow:0px 5px 40px #1111" class="navbar position-fixed navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand-logo" href="#">
-            <img src="logo.png" style="width: 200px; height: 100px" alt="Book Review System Logo" />
+            <img src="logo.png" style="width: 170px; height: 80px" alt="Book Review System Logo" />
         </a>
 
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <div class="collapse d-flex justify-content-center navbar-collapse justify-content-end" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item active">
                     <a class="nav-link" href="index.php"><i class="fa-solid fa-house-chimney mx-2"></i>Home</a>
@@ -26,7 +26,10 @@
                 <li class="nav-item hide-in-large">
                     <a class="nav-link" href="Profile.php">Profile</a>
                 </li>
-                <li class="nav-item account">
+               
+            </ul>
+        </div>
+        <li class="nav-item d-flex justify-content-center account">
                     <a href="Profile.php">
                         <div class="avatar">
                             <img src="../image/<?php if (empty($userimg)) {
@@ -38,6 +41,7 @@
                     </a>
 
                 </li>
-            </ul>
-        </div>
     </nav>
+    <div style="width: 100%;height: 100px;">
+        Nav
+    </div>
